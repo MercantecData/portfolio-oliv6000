112cdccb11931d5374fe957f0ee5cbc03e659588
Oliver <OIO.skp@edu.mercantec.dk>
2019-11-11 14:54:40 +0100

    added a watch top right

--- My portfolje/mitscript.js
+++ My portfolje/mitscript.js
@@ -2,6 +2,11 @@
 
 function buttonClicked(t)
 {
-    console.log("click!");
-    t.innerHTML = "clicked";
+    var SetGetDate = document.getElementById('demo');
+    SetGetDate.innerHTML = new Date().getHours() + ":" + new Date().getMinutes();
+    var TimeToWait = 500;
+    setTimeout(function(){
+    buttonClicked();
+    }, TimeToWait);
+    
 }
