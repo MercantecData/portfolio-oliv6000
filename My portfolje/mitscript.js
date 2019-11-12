7a101b08188924e36d918d1467370de12be97e04
Oliver <OIO.skp@edu.mercantec.dk>
2019-11-12 13:11:37 +0100

    Js tasks completed, plus other ajustments to html

--- My portfolje/mitscript.js
+++ My portfolje/mitscript.js
@@ -1,12 +1,43 @@
 // function erstater string-void-int osv, den kan retunere alting eller ingenting
 
-function buttonClicked(t)
+function AddLiveTime(t)
 {
     var SetGetDate = document.getElementById('demo');
     SetGetDate.innerHTML = new Date().getHours() + ":" + new Date().getMinutes();
     var TimeToWait = 500;
     setTimeout(function(){
-    buttonClicked();
+    AddLiveTime();
     }, TimeToWait);
-    
+}
+
+function AddPicture()
+{
+    document.getElementById('OliverPicture').style.display = "block";
+}
+
+function RemovePicture()
+{
+    document.getElementById('OliverPicture').style.display = "none";
+}
+
+
+
+// Here i change the data within the button with the id "ChangeTekst"
+function ChangeButtonTekst(t)
+{
+    document.getElementById('ChangeTekst').innerHTML = "hey there";
+}
+
+
+function AddArray()
+{
+    var MineSpil = ["GTA 5" ," CsGo", " ARK Survival evolved & and much more!"];
+
+    document.getElementById('MyGames').innerText = MineSpil;
+}
+
+function AddComent()
+{
+    var usercomment = document.getElementById('UserCommentt').value;
+    document.getElementById('UserCommentPlaceHold').innerText = usercomment;
 }
