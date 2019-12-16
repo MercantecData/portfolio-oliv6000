1313bb73fdeba9fe545b2c5402712cfdeb6454ea
Oliver <OIOl.skp@edu.mercantec.dk>
2019-12-16 13:22:56 +0100

    First push with the project task at hand
    Basically just created my files, and i uploaded  the task overview, word document and IMG of draw.io file UML

---
+++ H1 projektuge (Programmør)/H1_Projektuge_Opgave_SPIL/H1_Projektuge_Opgave_SPIL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace H1_Projektuge_Opgave_SPIL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ H1 projektuge (Programmør)/H1_Projektuge_Opgave_SPIL/H1_Projektuge_Opgave_SPIL/Weapon.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace H1_Projektuge_Opgave_SPIL
+{
+    class Weapon
+    {
+    }
+}
